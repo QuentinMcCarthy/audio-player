5a9e826613f0dd556f05bfb77d318b1864ef871b
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-12-14 10:01:14 +1300

    Setup expiry renewal

--- includes/header.php
+++ includes/header.php
@@ -1,25 +1,33 @@
 <?php
 	session_start();
 
+	$path = "./uploads/audio";
+
+	$dir = array_splice(scandir($path), 2, 1);
+
+	print_r($dir);
+
 	if(isset($_SESSION['playlist'])){
 		$playlist = $_SESSION['playlist'];
 
-		print_r($playlist);
-
-		for($i=0; $i < count($playlist) ; $i++){
+		for($i=0; $i < count($playlist); $i++){
 			$playlistItem = $playlist[$i];
 
-			list($time, $songname) = explode("-", $playlistItem);
+			list($time, $name) = explode("-", $playlistItem);
 
-			$expiry = strtotime("+30 day", $time);
+			rename($path.$playlistItem, $path.time().$name);
+		}
+	}
 
-			echo "<br /><br />".($expiry - time());
+	for($i=0; $i < ; $i++){
+		$file = $dir[$i];
 
-			if(time()>$expiry){
-				if(unlink("./uploads/audio/".$playlistItem)){
-					array_splice($_SESSION['playlist'], $i, 1);
-				}
-			}
+		list($time, $name) = explode("-", $file);
+
+		$expiry = strtotime("+30 day", $time);
+
+		if(time()>$expiry){
+			unlink("./uploads/audio/".$file)
 		}
 	}
 ?>
