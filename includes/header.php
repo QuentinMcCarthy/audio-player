6c3c913f603cd5778f13941c3dbdb71b70768de8
QuentinMcCarthy <qmccarthy97@gmail.com>
2019-05-18 13:19:40 +1200

    Commented out unused code

--- includes/header.php
+++ includes/header.php
@@ -1,35 +1,35 @@
 <?php
-	session_start();
-
-	$path = "./uploads/audio";
-
-	$dir = array_splice(scandir($path), 2, 1);
-
-	print_r($dir);
-
-	if(isset($_SESSION['playlist'])){
-		$playlist = $_SESSION['playlist'];
-
-		for($i=0; $i < count($playlist); $i++){
-			$playlistItem = $playlist[$i];
-
-			list($time, $name) = explode("-", $playlistItem);
-
-			rename($path.$playlistItem, $path.time().$name);
-		}
-	}
-
-	for($i=0; $i < ; $i++){
-		$file = $dir[$i];
-
-		list($time, $name) = explode("-", $file);
-
-		$expiry = strtotime("+30 day", $time);
-
-		if(time()>$expiry){
-			unlink("./uploads/audio/".$file)
-		}
-	}
+	// session_start();
+	//
+	// $path = "./uploads/audio";
+	//
+	// $dir = array_splice(scandir($path), 2, 1);
+	//
+	// print_r($dir);
+	//
+	// if(isset($_SESSION['playlist'])){
+	// 	$playlist = $_SESSION['playlist'];
+	//
+	// 	for($i=0; $i < count($playlist); $i++){
+	// 		$playlistItem = $playlist[$i];
+	//
+	// 		list($time, $name) = explode("-", $playlistItem);
+	//
+	// 		rename($path.$playlistItem, $path.time().$name);
+	// 	}
+	// }
+	//
+	// for($i=0; $i < ; $i++){
+	// 	$file = $dir[$i];
+	//
+	// 	list($time, $name) = explode("-", $file);
+	//
+	// 	$expiry = strtotime("+30 day", $time);
+	//
+	// 	if(time()>$expiry){
+	// 		unlink("./uploads/audio/".$file)
+	// 	}
+	// }
 ?>
 
 <!DOCTYPE html>
